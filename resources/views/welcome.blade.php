a845453beb3259e6687e6d3b9ccde7f28780cf5c
victor leon <vdleon30@gmail.com>
2024-03-18 02:57:32 -0300

    Fixes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,12 +34,6 @@
                                         Dashboard
                                     </a>
 
-                                     <a
-                                        href="{{ route("events") }}"
-                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                    >
-                                        Eventos
-                                    </a>
                                 @else
                                     <a
                                         href="{{ route('login') }}"
